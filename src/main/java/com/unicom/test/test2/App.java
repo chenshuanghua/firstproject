9e44b6b526ecc7aa906bf34b9ca11a7758f8cea7
chensh <820771814@qq.com>
2018-11-15 09:02:56 +0800

    test2.3

--- src/main/java/com/unicom/test/test2/App.java
+++ src/main/java/com/unicom/test/test2/App.java
@@ -1,6 +1,7 @@
 package com.unicom.test.test2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.unicom.test.test2.App;
 
@@ -8,8 +9,8 @@ import com.unicom.test.test2.App;
  * Hello world!
  *
  */
-public class App 
-{
+@SpringBootApplication
+public class App {
     public static void main( String[] args ){
     	SpringApplication.run(App.class, args);
         //System.out.println( "Hello World!" );
